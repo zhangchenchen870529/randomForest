80a4ef9aa88900aad3289e33e56a74351bc634b6
zcc <40755917+zhangchenchen870529@users.noreply.github.com>
2024-05-28 14:03:34 +0800

    Delete test directory

--- test/work.sh
+++
@@ -1,6 +0,0 @@
-Rscript ../bin/randomForest/randomForest.R ../input/bacteria/train.x ../input/bacteria/train.y ../input/bacteria/test.x ../input/bacteria/test.y 5 0.9 5 0 bacteria 0
-
-Rscript ../bin/randomForest/randomForest.R ../input/metabolites/train.x ../input/metabolites/train.y ../input/metabolites/test.x ../input/metabolites/test.y 5 0.9 5 0 metabolites 0
-
-Rscript ../bin/randomForest/randomForest.R ../input/combined/train.x ../input/combined/train.y ../input/combined/test.x ../input/combined/test.y 5 0.9 5 0 combined 0
-
